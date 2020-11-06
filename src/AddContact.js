f0ead0e62e7ffe1c92b2ba2d30b6e3c37e43b879
MohcineDev <mouhcinedev@gmail.com>
2020-11-06 11:35:40 +0000

    reset state - add and check input value

--- src/AddContact.js
+++ src/AddContact.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 export default function AddContact(props) {
 
@@ -8,30 +8,61 @@ export default function AddContact(props) {
         tel: ''
     })
 
-    useEffect(() => {
-        document.querySelector('#name').focus()
-    }, [])
-
     const handleChange = (e) => {
         setContact({ ...Contact, [e.target.id]: e.target.value })
+    }
+
+    const checkInputs = () => {
+        props.addContact(Contact)
+
+        setContact({
+            name: '',
+            email: '',
+            tel: ''
+        })
+        document.querySelector('#name').focus()
     }
 
     const handleSubmit = (e) => {
         e.preventDefault()
         Contact.id = Math.floor(Math.random() * 10000);
-        props.addContact(Contact)
-        e.target.children[0].value = ''
-        e.target.children[1].value = ''
-        e.target.children[2].value = ''
-        document.querySelector('#name').focus()
+
+        if (Contact.name.trim() === '') {
+            document.querySelector('.fullname span').style.display = 'block'
+            return
+        }
+        else if (Contact.email.trim() === '') {
+            document.querySelector('.email span').style.display = 'block'
+            return
+        }
+        else if (Contact.tel.trim() === '') {
+            document.querySelector('.tel span').style.display = 'block'
+            return
+        }
+        else {
+            checkInputs()
+        }
+  
+    }
+    const handleFocus = (e) => {
+        e.target.nextSibling.style.display='none'
     }
 
     return <>
         <div className="wrapper">
             <form onSubmit={(e) => handleSubmit(e)}>
-                <input type="text" id="name" placeholder="full name..." autoFocus={true} onChange={(e) => handleChange(e)} />
-                <input type="email" id='email' placeholder="email..." onChange={(e) => handleChange(e)} />
-                <input type="text" id="tel" placeholder="tel..." onChange={(e) => handleChange(e)} />
+                <div className="fullname">
+                    <input type="text" id="name" placeholder="full name..." autoFocus={true} onFocus={e => handleFocus(e)} value={Contact.name} onChange={(e) => handleChange(e)} />
+                    <span>Enter Your Name</span>
+                </div>
+                <div className="email">
+                    <input type="email" id='email' placeholder="email..." onFocus={e => handleFocus(e)} value={Contact.email} onChange={(e) => handleChange(e)} />
+                    <span>Enter Your Email</span>
+                </div>
+                <div className="tel">
+                    <input type="tel" id="tel" placeholder="tel..." onFocus={e => handleFocus(e)} value={Contact.tel} onChange={(e) => handleChange(e)} />
+                    <span>Enter Your Tel</span>
+                </div>
                 <input type="submit" value="Submit" />
             </form>
         </div>
